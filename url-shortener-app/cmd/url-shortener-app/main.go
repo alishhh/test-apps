dfc960324e1134b4cae0fbd9c9547c801ae3ae44
Alisher Kuttybai <65455813+alishhh@users.noreply.github.com>
2023-12-26 12:49:12 +0600

    Update main.go

--- url-shortener-app/cmd/url-shortener-app/main.go
+++ url-shortener-app/cmd/url-shortener-app/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/alishhh/url-shortener-app/internal/app"
+import (
+	"github.com/alishhh/url-shortener-app/internal/app"
+)
 
 func main() {
 	application := app.New()
